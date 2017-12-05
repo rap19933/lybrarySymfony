1a5ce92f3391ff62c24e8f622193b983e4f59630
matyashov <matyashov@intaro.email>
2017-12-05 11:56:08 +0300

    add register

---
+++ src/LybraryBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LybraryBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="`user`")
+ * @ORM\Entity(repositoryClass="LybraryBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
